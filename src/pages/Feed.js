1dbe3b38bbb745378891e2ea2ea19df0cf015f26
Muhammad Zain <ztheleader@gmail.com>
2021-05-16 03:21:17 +0500

    Adds dummy stories to check scrolling.

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -10,6 +10,7 @@ const Feed = () => {
         <div className="listing-wrapper">
           <div className="row">
             <div className="col-sm-12">
+              
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
@@ -20,6 +21,67 @@ const Feed = () => {
                   </p>
                 </div>
               </div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
+                  <p className="card-text truncate">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled</p>
+                  <p className="card-footer">
+                    <span>1 min ago</span>
+                    <span>50 Comments</span>
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
+                  <p className="card-text truncate">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled</p>
+                  <p className="card-footer">
+                    <span>1 min ago</span>
+                    <span>50 Comments</span>
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
+                  <p className="card-text truncate">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled</p>
+                  <p className="card-footer">
+                    <span>1 min ago</span>
+                    <span>50 Comments</span>
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
+                  <p className="card-text truncate">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled</p>
+                  <p className="card-footer">
+                    <span>1 min ago</span>
+                    <span>50 Comments</span>
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
+                  <p className="card-text truncate">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled</p>
+                  <p className="card-footer">
+                    <span>1 min ago</span>
+                    <span>50 Comments</span>
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Lorem Ipsum is simply dummy text.</h5>
+                  <p className="card-text truncate">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled</p>
+                  <p className="card-footer">
+                    <span>1 min ago</span>
+                    <span>50 Comments</span>
+                  </p>
+                </div>
+              </div>
+              
             </div>
           </div>
         </div>
