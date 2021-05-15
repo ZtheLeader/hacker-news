b4afcedfc92404fd13c3dd17511a0b283dc67306
Muhammad Zain <ztheleader@gmail.com>
2021-05-16 02:29:54 +0500

    Basic structure for Feed.

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -2,13 +2,20 @@ const Feed = () => {
   return (
     <div className="main-wrapper">
       <header>
-
+        <div className="nav">
+          <img src="../assets/images/hackernews.svg" alt="hackernews-logo" />
+        </div>
       </header>
       <main>
 
       </main>
       <footer>
-
+        <div className="text-center">
+          <button className="btn btn-success">Load More</button>
+        </div>
+        <div className="footer-logo">
+          <img src="../assets/images/hackernews-footer.svg" alt="footer logo" />
+        </div>
       </footer>
     </div>
   )
