48a72b5030280ae59fabeed6bf457f91d82ae31d
Muhammad Zain <ztheleader@gmail.com>
2021-05-16 04:33:23 +0500

    Adds a function to create chuncks of Ids to be used in pagination. Also adds sass dependency..

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -13,9 +13,24 @@ const Feed = () => {
   // State
   const [storyIds, setStoryIds] = useState([])
   const [storiesType, setStoriesType] = useState('new')
-  
+  const [currentPage, setCurrentPage] = useState(1)
+  // Constants
+  const storiesPerPage = 5  
 
   // Methods
+  /**
+   * Creates and returns a chunked array based on currentPage and storiesPerPage
+   * to be used for frontend based pagination as API doesn't give paginated response
+   * 
+   * @returns Array
+   */
+  const getCurrentChunk = () => {
+    const indexOfLastPostIds = currentPage * storiesPerPage;
+    const indexOfFirstPostIds = indexOfLastPostIds - storiesPerPage;
+    setCurrentPage(currentPage + 1)
+    const currentPostIds = storyIds.slice(indexOfFirstPostIds, indexOfLastPostIds);
+    return currentPostIds
+  }
 
   // Effects
   useEffect(() => {
@@ -25,9 +40,7 @@ const Feed = () => {
   }, [])
 
   useEffect(() => {
-    getStoryIds(storiesType).then(storyIds => {
-      setStoryIds(storyIds)
-    })
+    console.log(getCurrentChunk())
   }, [storiesType])
 
   // Template
