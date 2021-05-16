c3e883fa65af2ced1022182a21ce22c16a484b3b
Muhammad Zain <ztheleader@gmail.com>
2021-05-16 06:16:57 +0500

    Removes truncate class as in the mockups we have multiple lines.

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -3,7 +3,7 @@ const Story = ({ title, body, time, commentsCount, storyType }) => {
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text truncate">{body}</p>
+        <p className="card-text">{body}</p>
         <p className="card-footer">
           <span>{time}</span>
           {storyType !== 'job' &&
